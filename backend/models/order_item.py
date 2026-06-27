1e6c5c03860f9ac7e0af166e33968a3c5ffc9874
samsonmorara-maker/samsongit config  --global user.email samson.morara@student.moringaschool.com <samson.morara@student.moringaschool.com>
2026-06-27 14:05:20 +0300

    nothing much

--- backend/models/order_item.py
+++ backend/models/order_item.py
@@ -8,44 +8,37 @@ class OrderItem(Base):
 
     __tablename__ = "order_items"
 
-
     id = Column(
         Integer,
         primary_key=True,
         index=True
     )
 
-
     order_id = Column(
         Integer,
         ForeignKey("orders.id")
     )
-
 
     product_id = Column(
         Integer,
         ForeignKey("products.id")
     )
 
-
     quantity = Column(
         Integer,
         default=1
     )
-
 
     price = Column(
         Float,
         nullable=False
     )
 
-
     # Connect to Order
     order = relationship(
         "Order",
         back_populates="items"
     )
-
 
     # Connect to Product
     product = relationship(
